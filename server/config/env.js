8f08732c7e6dfd4b4df92561b4b65003243fdf4a
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-30 08:55:26 +0530

    Add Mongoose dependency and update environment configuration

--- server/config/env.js
+++ server/config/env.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const config = {
-port: process.env.PORT,
+export const credentials = {
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET,
 };
