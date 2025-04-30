04a02d5ee5c7574ff125f782bafe4570d14df86e
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-04-30 08:56:21 +0530

    Add createProject function to handle project creation with user limits

---
+++ server/controllers/projectController.js
@@ -0,0 +1,20 @@
+import Project from "../models/ProjectModel";
+
+export const createProject = (req, res) => {
+  const { title } = req.body;
+  const userId = req.userId;
+
+  try {
+    const existingProjects = Project.countDocuments({ user: userId });
+    if (existingProjects >= 4) {
+      return res
+        .status(400)
+        .json({ message: "Cannot create more than 4 projects" });
+    }
+
+    const project = new Project.create({ title, user: userId });
+    res.status(201).json({ project, message: "Project Created Successfully" });
+  } catch (error) {
+    res.status(501).json({ message: error.message });
+  }
+};
